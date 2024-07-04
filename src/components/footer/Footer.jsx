a3dfd7bc07986e9d4585811120a66ed0802fd8e1
annah11 <hanamesfin67@gmail.com>
2024-07-04 20:09:35 +0300

    perago

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,9 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>lamadev</div>
+      <div className={styles.logo}>perago</div>
       <div className={styles.text}>
-        Lama creative thoughts agency © All rights reserved.
+        Perago Information Technology© All rights reserved.
       </div>
     </div>
   );
